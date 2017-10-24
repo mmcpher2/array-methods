d7b49b6c7a921c6f12df04d89f49ccc49da9462f
Ryan Mcpherson <mmcpher2@gmail.com>
2017-10-24 12:00:43 -0500

    Comleted first part of excersize

--- array-methods.js
+++ array-methods.js
@@ -1,13 +1,18 @@
 
-
 const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
-
 /*
     Use the forEach method to add the name of each planet
     to a section element in your HTML with an id of "planets".
     Use string templates to construct the DOM elements.
 */
 const planetEl = document.getElementById("planets")
+
+const insertPlanets = function (planets) {
+    planetEl.innerHTML += `
+    <article>${planets}</article>
+    `
+}
+planets.forEach(insertPlanets)
 
 /*
     Use the map method to create a new array where the 
@@ -16,7 +21,14 @@ const planetEl = document.getElementById("planets")
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
-
+const planetCapitalizer = planets.map(function (planet) {
+        const firstLetter = planet.charAt(0)
+        const capitalFl = firstLetter.toUpperCase()
+        const brokenPlanet = planet.replace(firstLetter, "")
+        const fixedPlanet = capitalFl + brokenPlanet
+        return fixedPlanet
+})
+console.log(planetCapitalizer)
 
 /*
     Use the filter method to create a new array that
@@ -26,6 +38,19 @@ const planetEl = document.getElementById("planets")
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 */
 
+const eLetterPlanets = planets.filter(function (ePlanets) {
+        const filteredPlanets = ePlanets.includes("e")
+        if (filteredPlanets === true) {
+            return ePlanets
+        }
+})
+console.log(eLetterPlanets)
 
 // Use the reduce method to create a sentence from the words in the following array
 const words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"]
+
+const newSentence = words.reduce(function (allWords, eachWord){
+        const sentenceBuilder = allWords + " " + eachWord
+        return sentenceBuilder
+})
+console.log(newSentence)
